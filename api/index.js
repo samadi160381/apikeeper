08b8028eff7ab0166b15089c09aee69fb55927a0
samadi160381 <dadamoh35@gmail.com>
2026-07-03 03:00:28 +0000

    Merge branch 'main' of https://github.com/samadi160381/apikeeper

---
+++ api/index.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    status: 'ok',
+    service: 'apikeeper',
+    message: 'API is running. See /api/matches for match data.'
+  });
+}
